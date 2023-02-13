562be198a0828b87fba533b348a371e190e326de
coldsober irene <82769571+coldsober-irene@users.noreply.github.com>
2023-02-13 08:58:57 +0200

    Update saving_cv2_video.py

--- saving_cv2_video.py
+++ saving_cv2_video.py
@@ -1,17 +1,15 @@
 import cv2
 
 # define the codec and create a VideoWriter object
+cap = cv2.VideoCapture(0)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
 
 # capture frames from a camera or load them from a file
-for i in range(100):
-    ret, frame = cap.read()
-    if ret == True:
-        # write the frame to the output video file
+while True:
+    success, frame = cap.read()
+    if success:
         out.write(frame)
-    else:
-        break
 
 # release the VideoWriter object
 out.release()
